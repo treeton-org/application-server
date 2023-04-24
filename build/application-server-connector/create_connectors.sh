20bd29f845e3d8d9e75d5b7bf8ffb7d0fede6c0a
Kok Kek <kokkekpek@gmail.com>
2023-04-24 08:16:19 +0300

    refactor: application-server-connector entrypoint

--- build/application-server-connector/create_connectors.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-for connector in $(ls /work/connectors)
-do
-    java -jar /usr/bin/connect-cli create -e http://connect:8083/  $(awk -F "." '{print $1}' <<< ${connector}) < /work/connectors/${connector}
-    sleep 10s
-done
